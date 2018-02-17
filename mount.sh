b64bb27123cbfb1744a936b7075415eea2748b8a
Paul Mansfield <pmansfield@gracenote.com>
2018-02-18 09:45:04 +1100

    make the solution more rebust, detect only ntfs volumes, remount them all the a local mount point within the home folder

--- mount.sh
+++ mount.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-
-var=$(diskutil info /Volumes/Untitled/ | grep "Device Node:" | awk '$1 == "Device" {print $3}');
-diskutil unmount $var;
-sudo /sbin/mount_ntfs -o rw,auto,nobrowse $var ~/ntfs-volume;
-open ~/ntfs-volume;
-
+for i in $( ls /Volumes ); do
+	
+	diskident=$(diskutil info /Volumes/$i | grep "Device Node:" | awk '$1 == "Device" {print $3}'| xargs);
+	filesys=$(diskutil info /Volumes/$i | grep "Partition Type:" | awk '$1 == "Partition" {print $3}' | xargs);
+	diskutil info /Volumes/$i | grep "Partition Type:" | awk '$1 == "Partition" {print $3}'
+	echo $i;
+	echo $diskident;
+	echo $filesys;
+	if [[ -n $diskident ]] && [[ $filesys = "Windows_NTFS" ]]; then
+		diskutil unmount $diskident;
+		mkdir -p ~/Volumes/$i;
+		sudo /sbin/mount_ntfs -o rw,auto,nobrowse $diskident ~/Volumes/$i;
+		open ~/Volumes/$i;
+	fi
+done
